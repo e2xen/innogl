6e48f32d92f8cec60a1127fc92d283f2078ac518
e2xen <max.ks1230@gmail.com>
2021-09-06 22:21:56 +0300

    init commit

---
+++ innogl/src/test/java/innogl/ru/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package innogl.ru.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
