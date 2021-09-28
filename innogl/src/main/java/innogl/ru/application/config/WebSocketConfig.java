f72735ea0655995e9aa357959f0efae02bb3c786
e2xen <max.ks1230@gmail.com>
2021-09-28 22:45:20 +0300

    add chat interceptor

--- innogl/src/main/java/innogl/ru/application/config/WebSocketConfig.java
+++ innogl/src/main/java/innogl/ru/application/config/WebSocketConfig.java
@@ -1,10 +1,13 @@
 package innogl.ru.application.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import innogl.ru.application.interceptor.ChatChannelInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.DefaultContentTypeResolver;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -15,7 +18,11 @@ import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final ChatChannelInterceptor chatChannelInterceptor;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker( "/chat");
@@ -40,5 +47,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         converter.setContentTypeResolver(resolver);
         messageConverters.add(converter);
         return false;
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(chatChannelInterceptor);
     }
 }
--- innogl/src/main/java/innogl/ru/application/controller/ChatController.java
+++ innogl/src/main/java/innogl/ru/application/controller/ChatController.java
@@ -2,7 +2,6 @@ package innogl.ru.application.controller;
 
 import innogl.ru.application.model.ChatMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -15,7 +14,7 @@ public class ChatController {
     private final SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/send-message")
-    public void processMessage(@Payload ChatMessage chatMessage) {
+    public void sendMessage(@Payload ChatMessage chatMessage) {
 
         messagingTemplate.convertAndSendToUser(
                 chatMessage.getChatId().toString(),"/queue/messages",
