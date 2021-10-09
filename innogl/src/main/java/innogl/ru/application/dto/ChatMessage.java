96820ead8883270f770a02feefb5b0d15b7c2bc2
e2xen <max.ks1230@gmail.com>
2021-10-10 00:23:14 +0300

    fix deserialization, add test

--- innogl/src/main/java/innogl/ru/application/dto/ChatMessage.java
+++ innogl/src/main/java/innogl/ru/application/dto/ChatMessage.java
@@ -1,12 +1,16 @@
 package innogl.ru.application.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatMessage {
     private UUID id;
     private UUID chatId;
--- innogl/src/test/java/innogl/ru/application/ApplicationTests.java
+++ innogl/src/test/java/innogl/ru/application/ApplicationTests.java
@@ -1,5 +1,6 @@
 package innogl.ru.application;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws JsonProcessingException {
 	}
 
 }
